36fc846d6dded851f99e03808976588f9f1db1d4
uiuuoq <h45778321@gmail.com>
2026-03-01 19:42:07 +0900

    DP-158 프로젝트 전체 라우팅 폴더 구조 설계 및 초기화

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-xl font-medium">로그인 페이지(/)입니다.</h1>
+      <h1 className="text-xl font-medium">커뮤니티 목록 페이지입니다.</h1>
     </div>
   );
 }
